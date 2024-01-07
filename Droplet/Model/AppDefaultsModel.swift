0a172373bb3256834be938a14b9b03907902406a
Turner Naef <Turner.Naef@gmail.com>
2024-01-06 23:23:22 -0500

    UserDefaults model (ik abt keychain giv time pls)

---
+++ Droplet/Model/AppDefaultsModel.swift
@@ -0,0 +1,15 @@
+//
+//  AppDefaultsModel.swift
+//  Droplet
+//
+//  Created by Turner Naef on 1/6/24.
+//
+
+import Foundation
+
+struct AppDefaultsModel {
+    let username: String
+    let email: String
+    let password: String
+    let jwtToken: String
+}
